a83cc0eac3259dd8e8ed05a2e3b073641141f844
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-07 20:04:19 +0000

    master - f3253fa updated v of node packages

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -17,7 +17,7 @@
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-4b91108');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-f3253fa');
 
 exports.VERSION = VERSION;
 
