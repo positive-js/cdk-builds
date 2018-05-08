096e0ced2601dc0b76e032b4ba5a8b4b8555a30f
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-08 19:45:25 +0000

    master - 4bb3c38 fix(ci): fixed variable

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('0.0.1-01818cd');
+const /** @type {?} */ VERSION = new Version('0.0.1-4bb3c38');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('0.0.1-01818cd');
+var /** @type {?} */ VERSION = new Version('0.0.1-4bb3c38');
 
 /**
  * @fileoverview added by tsickle
