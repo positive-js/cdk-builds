28c7e72a18cc88e159c90ebe24d54aab95f13604
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-10 20:32:19 +0000

    master - 7024a4b feat(ci): updated circle ci config

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -17,7 +17,7 @@
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-c83e0ce');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-7024a4b');
 
 exports.VERSION = VERSION;
 
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('0.0.1-c83e0ce');
+var /** @type {?} */ VERSION = new Version('0.0.1-7024a4b');
 
 /**
  * @fileoverview added by tsickle
