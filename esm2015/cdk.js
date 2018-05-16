721f6f4a8da24b303167c8ca14c627731f8c0c4b
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-16 10:05:09 +0000

    master - 611413d readme fixed

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('0.0.1-6ed5311');
+const /** @type {?} */ VERSION = new Version('0.0.1-611413d');
 
 /**
  * @fileoverview added by tsickle
