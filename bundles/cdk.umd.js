f70cbf81d2af9d386964c48c58fec4402b3c261f
lkramarov <lkramarov@ptsecurity.com>
2018-05-23 15:14:50 +0000

    master - a55ced4 feat(typography): Typography (#144638)

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -17,7 +17,7 @@
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-293766c');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-a55ced4');
 
 exports.VERSION = VERSION;
 
