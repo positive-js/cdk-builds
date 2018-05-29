6234cffc9c5ea29afcbb093909f88f7a6a79fd2b
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-29 21:27:07 +0000

    master - e2022cf revert module id ^

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -17,7 +17,7 @@
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-0084616');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-e2022cf');
 
 exports.VERSION = VERSION;
 
