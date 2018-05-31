129d3f4d47e814a9ed2ea1ff36115e6f84542653
Oleg Pimenov <pimenov.o@gmail.com>
2018-05-31 12:12:12 +0000

    master - 50c0125 fixed build

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-const /** @type {?} */ VERSION = new Version('0.0.1-d432765');
+const /** @type {?} */ VERSION = new Version('0.0.1-50c0125');
 
 /**
  * @fileoverview added by tsickle
--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -13,7 +13,7 @@ import { Version } from '@angular/core';
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new Version('0.0.1-d432765');
+var /** @type {?} */ VERSION = new Version('0.0.1-50c0125');
 
 /**
  * @fileoverview added by tsickle
