080609ad49706f1d3eb9fad8d90ca7e40aa5685f
Oleg Pimenov <pimenov.o@gmail.com>
2018-06-07 14:22:46 +0000

    master - 58ac0fa feat(badges): added inline badges

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -17,7 +17,7 @@
 /**
  * Current version of the Component Development Kit.
  */
-var /** @type {?} */ VERSION = new core.Version('0.0.1-bb26ca1');
+var /** @type {?} */ VERSION = new core.Version('0.0.1-58ac0fa');
 
 exports.VERSION = VERSION;
 
