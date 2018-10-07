ba5b7eff5065ebadbfa44d9c47227dcadb9949cc
Oleg Pimenov <pimenov.o@gmail.com>
2018-10-07 07:51:09 +0000

    master - 96d5f07 chore(cdk): updated tslint

--- esm2015/coercion.js
+++ esm2015/coercion.js
@@ -20,6 +20,7 @@ function _isNumberValue(value) {
     // parseFloat(value) handles most of the cases we're interested in (it treats null, empty string,
     // and other non-number values as NaN, where Number just uses 0) but it considers the string
     // '123hello' to be a valid number. Therefore we also check if Number(value) is NaN.
+    // tslint:disable
     return !isNaN(parseFloat(value)) && !isNaN(Number(value));
 }
 
