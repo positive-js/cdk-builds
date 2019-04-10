2dd5b95e0e9df37d5a32cdceed011f618c6f962d
Oleg Pimenov <pimenov.o@gmail.com>
2019-04-10 10:55:44 +0000

    master - 791d50c chore: bump version to 1.0.0-beta.5 w/ changelog

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
  * Current version of the Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('1.0.0-beta.4');
+const VERSION = new Version('1.0.0-beta.5');
 
 /**
  * @fileoverview added by tsickle
