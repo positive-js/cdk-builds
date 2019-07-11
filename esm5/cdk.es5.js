cab906b038892d2975403b4fa44e684c3d2293ae
Oleg Pimenov <pimenov.o@gmail.com>
2019-07-11 20:54:10 +0000

    master - 01b9e7b chore: bump version to 8.0.0-beta.6 w/ changelog

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
  * Current version of the Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.0.0-beta.5');
+var VERSION = new Version('8.0.0-beta.6');
 
 /**
  * @fileoverview added by tsickle
