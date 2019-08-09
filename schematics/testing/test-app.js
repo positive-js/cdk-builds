ae778002b9c36aed253c756883ae9c927f95dbd9
Oleg Pimenov <pimenov.o@gmail.com>
2019-08-09 12:10:23 +0000

    master - 4f5cc53 chore: updated yarn lock and deps for schematics

--- schematics/testing/test-app.js
+++ schematics/testing/test-app.js
@@ -1,13 +1,23 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 /** Create a base app used for testing. */
-function createTestApp(runner, appOptions = {}) {
-    const workspaceTree = runner.runExternalSchematic('@schematics/angular', 'workspace', {
-        name: 'workspace',
-        version: '7.0.0',
-        newProjectRoot: 'projects'
+function createTestApp(runner, appOptions = {}, tree) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const workspaceTree = runner.runExternalSchematic('@schematics/angular', 'workspace', {
+            name: 'workspace',
+            version: '8.0.0',
+            newProjectRoot: 'projects'
+        }, tree);
+        return runner.runExternalSchematicAsync('@schematics/angular', 'application', Object.assign({ name: 'material' }, appOptions), workspaceTree).toPromise();
     });
-    return runner.runExternalSchematic('@schematics/angular', 'application', Object.assign({}, appOptions, { name: 'mosaic' }), workspaceTree);
 }
 exports.createTestApp = createTestApp;
 //# sourceMappingURL=test-app.js.map
--- typings/schematics/testing/test-app.d.ts
+++ typings/schematics/testing/test-app.d.ts
@@ -1,3 +1,4 @@
+import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 /** Create a base app used for testing. */
-export declare function createTestApp(runner: SchematicTestRunner, appOptions?: {}): UnitTestTree;
+export declare function createTestApp(runner: SchematicTestRunner, appOptions?: {}, tree?: Tree): Promise<UnitTestTree>;
