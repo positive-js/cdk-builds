88057248f5e99853938bddcebbc1f6546f9d521c
Oleg Pimenov <pimenov.o@gmail.com>
2019-11-05 19:35:05 +0000

    master - 724ac0c chore: bump version to 8.1.0 w/ changelog

--- bundles/cdk.umd.js
+++ bundles/cdk.umd.js
@@ -18,7 +18,7 @@
  * Current version of the Component Development Kit.
  * @type {?}
  */
-var VERSION = new core.Version('8.0.4');
+var VERSION = new core.Version('8.1.0');
 
 exports.VERSION = VERSION;
 
