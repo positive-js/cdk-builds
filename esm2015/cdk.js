18345743a7e09020bfdfc51aa10984d9f4f5887f
Oleg Pimenov <pimenov.o@gmail.com>
2019-12-03 09:45:55 +0000

    master - a5a10a8 chore: bump version to 8.2.0 w/ changelog

--- esm2015/cdk.js
+++ esm2015/cdk.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
  * Current version of the Component Development Kit.
  * @type {?}
  */
-const VERSION = new Version('8.1.3');
+const VERSION = new Version('8.2.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk.js.map
