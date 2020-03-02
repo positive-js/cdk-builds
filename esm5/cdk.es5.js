a670b4291c9b1c7955213140b8446487ee9d16dd
Oleg Pimenov <pimenov.o@gmail.com>
2020-03-02 11:32:14 +0000

    master - 54431372 chore: bump version to 8.4.2 w/ changelog

--- esm5/cdk.es5.js
+++ esm5/cdk.es5.js
@@ -14,7 +14,7 @@ import { Version } from '@angular/core';
  * Current version of the Component Development Kit.
  * @type {?}
  */
-var VERSION = new Version('8.4.1');
+var VERSION = new Version('8.4.2');
 
 export { VERSION };
 //# sourceMappingURL=cdk.es5.js.map
